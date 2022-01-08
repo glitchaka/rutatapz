d1ccdf21396c8947b507aaed34bf3a4f3dbc7d12
glitchaka <82622846+glitchaka@users.noreply.github.com>
2022-01-08 19:04:57 -0300

    page

---
+++ assets/push.bat
@@ -0,0 +1,7 @@
+echo "# rutatapz" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/glitchaka/rutatapz.git
+git push -u origin main
